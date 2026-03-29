026ebddef671e139bfe068512004ffe6184599a6
bruce <bruce@mellows.tv>
2026-03-29 17:10:43 +1100

    Added usings

--- EventuallyCorrectCacheTT.cs
+++ EventuallyCorrectCacheTT.cs
@@ -20,6 +20,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+using System;
+using System.Collections.Generic;
+
 namespace BruceMellows.EventuallyCorrect;
 
 public sealed class EventuallyCorrectCache<TKey, TValue>(Func<TKey, TValue> valueFactory, IEqualityComparer<TKey> comparer, Func<TKey, string>? identityFactory = default)
